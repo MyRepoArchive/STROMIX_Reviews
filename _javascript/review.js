7244dc23fe2af23c2600d75f7aba83acc65261ab
João Victor <joaovictor-san-co@hotmail.com>
2020-06-15 23:20:50 -0300

    Foi feita a aba de veja mais

---
+++ _javascript/review.js
@@ -0,0 +1,6 @@
+var alturaAside = document.querySelector('#outros_reviews').clientHeight
+if(window.innerWidth <= 360) {
+    document.querySelector('#corpo').style.paddingBottom = alturaAside+'px'
+}
+
+//Expandir imagens
